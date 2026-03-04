1fb9cb65ec90aa497d0b5ecd1805cbd1916a4776
Marco Rafael Simões Costa <markcostah@gmail.com>
2026-03-04 21:01:22 +0000

    refactor: enhance monthly statement generation with dataclass and improve data processing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import camelot
 import re
+from dataclasses import dataclass
+
+@dataclass
+class MontlyStatement:
+  debit: float
+  credit: float
+  net_balance: float
+  number_of_transactions: int
+  top_expenses: list
+  top_incomes: list
+  
+  def __init__(self, debit: float, credit: float, net_balance: float, number_of_transactions: int, top_expenses: list, top_incomes: list):
+    self.debit = debit
+    self.credit = credit
+    self.net_balance = net_balance
+    self.number_of_transactions = number_of_transactions
+    self.top_expenses = top_expenses
+    self.top_incomes = top_incomes
 
 def extract_table_from_pdf(filepath: str):
   date_pattern = r'\d{2}-\d{2}-\d{2}'
@@ -18,8 +36,9 @@ def extract_table_from_pdf(filepath: str):
   return rows
 
 def generate_df(rows:list) -> pd.DataFrame:
-  columns = ["Date","Date", "Description", "Debit","Credit", "Balance"]
-  df = pd.DataFrame(rows, columns=columns)
+  df = pd.DataFrame(rows)
+  df = df.drop(1, axis=1)
+  df.columns = ["Date", "Description", "Debit", "Credit", "Balance"]
   return df
 
 def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
@@ -35,10 +54,34 @@ def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
   df["Debit"] = df["Debit"].fillna("-")
   return df
 
+def generate_monthly_statement(df: pd.DataFrame) -> MontlyStatement:
+  top3_expenses = df[df["Debit"] != "-"].sort_values(by="Debit", ascending=False).head(3)
+  top3_incomes = df[df["Credit"] != "-"].sort_values(by="Credit", ascending=False).head(3)
+  transactions = len(df)
+  total_debit = df["Debit"].replace("-", 0).sum()
+  total_credit = df["Credit"].replace("-", 0).sum()
+  net_balance = total_credit - total_debit
+  
+  statement = MontlyStatement(
+    debit=round(total_debit, 2),
+    credit=round(total_credit, 2),
+    net_balance=round(net_balance, 2),
+    number_of_transactions=transactions,
+    top_expenses=top3_expenses,
+    top_incomes=top3_incomes
+  )
+  
+  return statement
+  
+
 if __name__ == "__main__":
   file_path = "statement.pdf" 
   rows = extract_table_from_pdf(file_path)
   df = generate_df(rows)
   
   cleaned_df = preprocess_data(df)
-  cleaned_df.to_csv("cleaned_data.csv", index=False)
+  generate_monthly_statement(cleaned_df)
+  
+  statement = generate_monthly_statement(cleaned_df)
+  
+  print("Statement: ", statement)
